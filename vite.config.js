84b79d91de489839b007e2d3c70c73176d254345
Ayodele-akinkunmi-joseph <fnalistboy@gmail.com>
2026-02-14 19:50:57 +0100

    Add publicDir configuration to vite.config.js
    This ensures files in public folder are copied to dist root.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // This ensures files in public folder are copied to dist root
+  publicDir: 'public'
 })
